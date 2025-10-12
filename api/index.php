d729ec927e326d8eb53fe2938d64a69eb4196b00
Yin Khin <you@example.com>
2025-10-12 19:01:44 +0700

    Fix vercel.json env vars and routes for D1/R2

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require __DIR__ . '/../vendor/autoload.php';
+// require __DIR__ . '/../vendor/autoload.php';
 
 // If you are using Laravel, forward requests to your public index.php
 require __DIR__ . '/../public/index.php';
